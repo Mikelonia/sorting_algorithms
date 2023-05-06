1629abb55f802415b173b1e6224d93e57b37531a
Mikelonia <onajitemike4real@gmail.com>
2023-05-06 05:18:26 -0700

    Task 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * using the selection sort algorithm by Okpako Michael
+ * @array: array
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, k, min, tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (k = i + 1; k < size; k++)
+		{
+			if (array[k] < array[min])
+				min = k;
+		}
+		if (min != i)
+		{
+			/*tmp = array[i];*/
+			array[i] = array[min];
+			tmp = array[i];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
+}
